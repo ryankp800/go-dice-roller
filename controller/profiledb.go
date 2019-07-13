b74021490de9cdeecc0659b8fa3778650cd34847
ryankp800 <ryankirkpatrick@gmail.com>
2019-07-13 14:33:46 -0500

    Update profiledb.go

--- controller/profiledb.go
+++ controller/profiledb.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"fmt"
+	"log"
 	"os"
 )
 
@@ -12,12 +14,16 @@ func GetDBCollection() (*mongo.Collection, error) {
 	// _ = os.Setenv("MONGODB_URI", "mongodb://localhost:27017")
 	//mongodb://heroku_qkwm7vgb:tqug715ledj81r2gs24ajqj4kj@ds213255.mlab.com:13255/heroku_qkwm7vgb
 
+	log.Println("profiledb ", os.Getenv("MONGODB_URI"))
+
 	clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_URI"))
 
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		return nil, err
 	}
+
+	fmt.Println("Connected to MongoDB! with profiledb")
 	// Check the connection
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
